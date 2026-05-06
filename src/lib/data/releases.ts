bef3e9abfd45db349af158916affd501586ef98c
Faneencik1 <ivanvyalkov170@gmail.com>
2026-05-07 00:46:42 +0300

    изменены релизы

--- src/lib/data/releases.ts
+++ src/lib/data/releases.ts
@@ -1,6 +1,6 @@
 export let releases = [
-    ['МНЕ ЛЕНЬ (TML Remix)', 'Arti Sun, klorbot, TMLYATL', "/releases/imLazyAlbum.png", "/releases/imLazy.mp3", false],
-    ['плачь для меня', 'отложу на завтра', "/releases/cryForMeAlbum.jpg", "/releases/cryForMe.mp3", false],
-    ['-Rep', 'klorbot', "/releases/minusRepAlbum.jpg", "/releases/minusRep.mp3", false],
-    ['Танцпол', 'Arti Sun', "/releases/dancefloorAlbum.jpeg", "/releases/dancefloor.mp3", false],
+    ['ИТД рэп', 'klorbot, CookieMail, gr1mor', "/releases/itdrap.png", "/releases/itdrap.mp3", false],
+    ['АЛЛО', 'CookieMail', "/releases/hello.png", "/releases/hello.mp3", false],
+    ['КУПЕР', 'klorbot, Курьер-звукарь', "/releases/kuper.png", "/releases/kuper.mp3", false],
+    ['НАХЛЕБНИКИ', 'Арт Мой Брат', "/releases/nahlebniki.png", "/releases/nahlebniki.mp3", false],
 ]
