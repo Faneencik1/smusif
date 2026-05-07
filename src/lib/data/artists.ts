57cf6a94252db87bf183526d9c147dae4f3f10a1
Faneencik1 <ivanvyalkov170@gmail.com>
2026-05-08 00:38:57 +0300

    add ivasbanas

--- src/lib/data/artists.ts
+++ src/lib/data/artists.ts
@@ -3,11 +3,12 @@ import klorbot from "$lib/assets/artist_avaatars/klorbot.png"
 import ArtMyBro from "$lib/assets/artist_avaatars/ArtMyBro.png"
 import CookieMail from "$lib/assets/artist_avaatars/CookieMail.png"
 import NikKat from "$lib/assets/artist_avaatars/NikKat.png"
+import IVASBANAS from "$lib/assets/artist_avaatars/IVASBANAS.png"
 
 export let artists = [
     ['Arti Sun', ArtiSun],
     ['Арт Мой Брат', ArtMyBro],
     ['CookieMail', CookieMail],
     ['klorbot', klorbot],
-    ["NikKat", NikKat],
+    ["IVASBANAS", IVASBANAS],
 ];
